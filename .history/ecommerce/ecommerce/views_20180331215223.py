14805ae4456fc21441569b532e223bfa1542fde9
Michael James Abadilla <mjmaix@gmail.com>
2018-06-10 12:39:04 +0800

    remove history

--- .history/ecommerce/ecommerce/views_20180331215223.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home_page(request):
-    return HttpResponse("<h1>Hello World</h1>")
--- .history/ecommerce/src/views_20180331221406.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-define home_page(request):
-    return HttpResponse("<h1>Hello World</h1>")
